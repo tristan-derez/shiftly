e03c894fedb64bcaaa01d28c85a30c6e2a1397e3
Tristan <tristan.derez@gmail.com>
2026-09-06 22:24:18 +0200

    fix: remove password from api user schema

--- packages/api/src/user.ts
+++ packages/api/src/user.ts
@@ -1,8 +1,9 @@
 import { Schema } from "effect";
 
 export const User = Schema.Struct({
-	name: Schema.String,
-	email: Schema.optional(Schema.String),
-	password: Schema.String,
+	name: Schema.String.pipe(Schema.maxLength(100)),
+	username: Schema.String.pipe(Schema.maxLength(30)),
+	email: Schema.optional(Schema.String.pipe(Schema.maxLength(254))),
 });
+
 export type User = typeof User.Type;
