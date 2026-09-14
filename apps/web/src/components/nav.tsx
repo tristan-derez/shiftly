e010ee279b0b489a2a2f1c4a8af7fb5b85d124b8
Tristan <tristan.derez@gmail.com>
2026-09-14 23:54:14 +0200

    feat: add logo

--- apps/web/src/components/nav.tsx
+++ apps/web/src/components/nav.tsx
@@ -1,26 +1,21 @@
-import { Link } from "@tanstack/react-router";
+import { Link, useRouteContext } from "@tanstack/react-router";
 import { UserMenu } from "@/components/user-menu";
-import { authClient } from "@/lib/auth-client";
 
-function Nav() {
-	const { data: authData } = authClient.useSession();
+export function Nav() {
+	const { authData } = useRouteContext({ from: "__root__" });
 
-	if (!authData) {
-		return null;
-	}
-
-	return (
-		<div className="flex items-center justify-between px-2 pt-4">
-			<div className="flex gap-2">
-				<Link to="/" className="[&.active]:font-bold">
-					Accueil
-				</Link>
+	return !authData?.user ? null : (
+		<header className="static top-0 z-10 bg-background">
+			<div className="mx-auto flex w-full items-center justify-between py-4">
+				<div className="flex gap-2">
+					<Link to="/" aria-label="Accueil">
+						<img src="/shiftly-full-logo.svg" className="h-5 w-auto" />
+					</Link>
+				</div>
+				<div className="flex items-center gap-2">
+					<UserMenu />
+				</div>
 			</div>
-			<div className="flex items-center gap-2">
-				<UserMenu />
-			</div>
-		</div>
+		</header>
 	);
 }
-
-export { Nav };
